c31c5fb1274c435e10c3164c62accc7067c0b136
Violetics <violetics.api@gmail.com>
2022-03-21 16:25:22 +0800

    add: 2 plugins

--- test/example.test.js
+++ test/example.test.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 const Violetics = require("../src");
 const canvas = new Violetics("beta");
 
-const VSNeon = new canvas.VSNeon().setPlayer2(fs.readFileSync("./media/violetics.png"));
+const VSSynthretro = new canvas.VSSynthretro().setPlayer2(fs.readFileSync("./media/violetics.png"));
 
-VSNeon.toBuffer((err, result) => {
+VSSynthretro.toBuffer((err, result) => {
 	if (err) return console.error(err);
 	let path = `./media/result.${result.type.ext}`;
 	fs.writeFileSync(path, result.buffer);
