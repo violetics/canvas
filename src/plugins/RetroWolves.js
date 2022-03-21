34365765227c5637c3ceccdd09cf6ee952d2a3d7
Violetics <violetics.api@gmail.com>
2022-03-21 18:58:58 +0800

    add: 3 plugins

---
+++ src/plugins/RetroWolves.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const path = require("path");
+
+function RetroWolves(self) {
+	let { VioleticsError, request, parseOptions } = self.utils;
+	class RetroWolves {
+		constructor(avatar) {
+			if (avatar && typeof avatar != "string")
+				throw new VioleticsError("avatar must be typeof string");
+			this.methods = Object.getOwnPropertyNames(RetroWolves.prototype);
+			this._args = { avatar: avatar };
+		}
+		setAvatar(avatar) {
+			if (!avatar || typeof avatar != "string")
+				throw new VioleticsError("setAvatar() required avatar and must be typeof string");
+			this._args.avatar = avatar;
+			return this;
+		}
+		send(apikey, options) {
+			return request(self.BASE("retro-wolves", apikey), options);
+		}
+		toBuffer(cb) {
+			let options = parseOptions(
+				{
+					avatar: fs.readFileSync(path.join(__dirname, "../../media/violetics.png")),
+				},
+				this._args
+			);
+			if (cb && typeof cb == "function") {
+				return this.send(self.apikey, options)
+					.then((buffer) => cb(null, buffer))
+					.catch((error) => cb(error, null));
+			}
+			return this.send(self.apikey, options);
+		}
+	}
+	return RetroWolves;
+}
+
+module.exports = RetroWolves;
--- test/example.test.js
+++ test/example.test.js
@@ -2,13 +2,12 @@ const fs = require("fs");
 const Violetics = require("../src");
 const canvas = new Violetics("beta");
 
-let q = new canvas.Quotes()
-    .setAuthor("Dehan")
-    .setText("made with fully automatic");
-
-q.toBuffer()
-    .then((result) => {
-        fs.writeFileSync("./media/result." + result.type.ext, result.buffer);
-        console.log("done");
-    })
-    .catch(console.error);
+let Retro = new canvas.RetroWolves()
+    .setAvatar("https://violetics.pw/assets/avatars/user.png");
+    
+Retro.toBuffer()
+	.then((result) => {
+		fs.writeFileSync("./media/result." + result.type.ext, result.buffer);
+		console.log("done");
+	})
+	.catch(console.error);
