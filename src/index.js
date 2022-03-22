2c539f65b9ffd1589793117367267c0bd80ff33f
Violetics <violetics.api@gmail.com>
2022-03-22 10:10:55 +0800

    add: name, version of package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const { name, version } = require("../package");
 const utils = require("./utils");
 const { VioleticsError } = utils;
 
@@ -6,6 +6,8 @@ class Violetics {
 	constructor(apikey) {
 		if (!apikey || typeof apikey != "string")
 			throw new VioleticsError("arguments 'apikey' must be typeof string and required!");
+		this.name = name;
+		this.version = version;
 		this.utils = utils;
 		this.plugins = require("./plugins");
 		this.apikey = apikey;
